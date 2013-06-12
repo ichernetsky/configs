61c8788d7c13cfc5ba66e3bbcc33675d6a9241bb
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2013-06-13 02:58:07 +0400

    Use master branch of el-get

--- emacs/init.el
+++ emacs/init.el
@@ -4,8 +4,9 @@
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
 
 (setq el-get-sources
       '((:name show-wspace
